7cd43098cc8610d755a13ce97d1eaafa5eb94be9
Pujan Shrestha <pujan@192-168-1-105.tpgi.com.au>
2025-04-18 11:04:39 +1000

    Delete chapter completed'

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -1,9 +1,13 @@
 "use client"
 
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
 import { boolean, string } from "zod";
 import { ConfirmModal } from "@/components/modals/confirm-modal";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 interface ChapterActionsProps {
     disabled: boolean,
@@ -19,17 +23,33 @@ export const ChapterActions = ({
     chapterId,
     isPublish
 }: ChapterActionsProps) => {
+    const [ isLoading, setIsLoading ] = useState( false );
+    const router = useRouter();
+    const onDelete = async () => {
+        try {
+            setIsLoading ( true );
+            await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+            toast.success("Chapter deleted.")
+            router.refresh;
+            router.push(`/teacher/courses/${courseId}`);
+        } catch (error) {
+            toast.error("Something went wrong");
+            console.log("error on delete ", error);
+        } finally {
+            setIsLoading ( false );
+        }
+    }
     return (
         <div className="flex items-center gap-x-2">
             <Button
                 onClick={()=>{}}
-                disabled={disabled}
+                disabled={disabled || isLoading}
                 variant="outline"
                 size="sm"
             >
                 {isPublish ? "Unpublish" : "Publish"}
             </Button>
-            <ConfirmModal onConfrim={()=>{}}>
+            <ConfirmModal onConfrim={onDelete}>
                 <Button size="sm">
                     <Trash className="h-4 w-4" />
                 </Button>
--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -12,10 +12,90 @@ if (!process.env.MUX_TOKEN_ID || !process.env.MUX_TOKEN_SECRET) {
     tokenSecret: process.env.MUX_TOKEN_SECRET!,
   });
 
+export async function DELETE(
+    req: Request,
+    { params }: { params: { courseId: string, chapterId: string } }
+){
+    try {
+        console.log("chapter id ", params.chapterId)
+        const { userId } = auth();
+        if ( !userId ) {
+            return new NextResponse("Unauthorized User", { status: 401 });
+        }
+
+        const ownCourse = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId,
+            }
+        });
+        if ( !ownCourse ) {
+            return new NextResponse("Unauthorized course owner ", { status: 401 } );
+        }
+
+        const chapter = await db.chapter.findUnique({
+            where: {
+                id: params.chapterId,
+                courseId: params.courseId
+            }
+        });
+        console.log("chapter detail ", chapter)
+        if ( !chapter ) {
+            return new NextResponse("Chapter Not Found", { status: 404 } );
+        }
+
+        if ( chapter.videoUrl ) {
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId,
+                }
+            });
+            if ( existingMuxData ) {
+                await mux.video.assets.delete( existingMuxData.assetId );
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id,
+                    }
+                });
+            }
+        }
+
+        
+        const deletedChapter = await db.chapter.delete({
+            where: {
+                id: params.chapterId
+            }
+        });
+
+        const publishedChaptersInCourse = await db.chapter.findMany({
+            where: {
+                courseId: params.courseId,
+                isPublish: true,
+            }
+        });
+
+        if ( !publishedChaptersInCourse.length ){
+            await db.course.update({
+                where: {
+                    id: params.courseId
+                },
+                data: {
+                    isPublish: false
+                }
+            });
+        }
+
+        return NextResponse.json(deletedChapter);
+
+    } catch (error) {
+        console.log("[ CHAPTER_ID_DELETE ]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
 
 export async function PATCH(
     req: Request,
-    { params }: { params : { courseId: string, chapterId: string }}
+    { params }: { params : { courseId: string, chapterId: string } }
 ){
     try {
         const { userId } = auth();
