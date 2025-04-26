909fffa76e8c1349abdce49b5c6e5e1067ab05a7
Pujan Shrestha <pujan@Pujans-MacBook-Pro-2.local>
2025-04-26 17:10:25 +1000

    pagination and row sorted

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,31 +1,28 @@
-import { Button } from '../../../../../components/ui/button';
-import Link from 'next/link';
+
+import { auth } from '@clerk/nextjs/server';
+import { db } from '@/lib/db';
+import { redirect } from 'next/navigation';
 import { DataTable } from './_components/data-table';
 import { columns } from './_components/columns';
-
-async function getData(): Promise<any[]> {
-    // Fetch data from your API here.
-    return [
-      {
-        id: "728ed52f",
-        amount: 100,
-        status: "pending",
-        email: "m@example.com",
-      },
-      // ...
-    ]
-  }
+import { NextResponse } from 'next/server';
 
 const CoursesPage = async () => {
-    const data = await getData();
+    const { userId } = auth();
+    if(!userId){
+        return redirect("/");
+    }
+    const courses = await db.course.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    })
+    
     return ( 
         <div className="p-6">
-            <Link href="/teacher/create">
-                <Button>
-                    New course
-                </Button>
-            </Link>
-            <DataTable columns={columns} data={data} />
+            <DataTable columns={columns} data={courses} />
 
         </div>
      );
