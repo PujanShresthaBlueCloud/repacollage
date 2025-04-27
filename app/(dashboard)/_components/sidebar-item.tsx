03ea9bae67e717999707f850922c8d338fdcffee
Pujan Shrestha <pujan@Pujans-MacBook-Pro-2.local>
2025-04-27 12:37:50 +1000

    Fix the sidebar isActive

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -17,7 +17,10 @@ export const SidebarItem = ({
     const pathname = usePathname();
     const router = useRouter();
 
-    const isActive = (pathname === "/" && href == "/") || pathname == '/' || pathname?.startsWith('${href}/');
+    const isActive = 
+        (pathname === "/" && href == "/") || 
+        pathname === href ||
+        pathname?.startsWith('${href}/');
 
     const onClick = () => {
         router.push(href)
@@ -25,11 +28,12 @@ export const SidebarItem = ({
 
     return (
         <button 
-        onClick={onClick}
-        type="button"
-        className={cn(
-            "flex items-center gap-x-20 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20", isActive &&  "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
-        )}
+            onClick={onClick}
+            type="button"
+            className={cn(
+                "flex items-center gap-x-20 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20", 
+                isActive &&  "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+            )}
         >
             <div className="flex items-center gap-x-2 py-4">
                 <Icon 
@@ -43,7 +47,7 @@ export const SidebarItem = ({
             </div>
             <div 
                 className={cn(
-                    "ml-auto opacity-0 border-2 boder-sky-700 h-full transition-all", 
+                    "ml-auto opacity-0 border-2 border-sky-700  h-full transition-all",
                     isActive && "opacity-100"
                 )}
             />
